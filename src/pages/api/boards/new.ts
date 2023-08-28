0ff586b7f4cb84ea4c12f23542983ffe5947b7d8
bholmesdev <bholmesdev@gmail.com>
2023-08-28 18:52:59 -0400

    feat: end-to-end board creation!

--- src/pages/api/boards/new.ts
+++ src/pages/api/boards/new.ts
@@ -1,15 +1,33 @@
+import { Board, Sound, db } from "@/lib/schema";
 import type { APIRoute } from "astro";
 import { z } from "zod";
+import { v4 as uuidv4 } from "uuid";
 
-const payload = z.object({
-  url: z.string().url(),
-});
+const payload = z.record(z.string().url());
 
 export const POST: APIRoute = async ({ request }) => {
-  const raw = await request.formData();
-  const formData = payload.parse(Object.fromEntries(raw));
+  const raw = await request.json();
+  const boardEntries = payload.parse(raw);
 
-  console.log(formData.url);
+  const [, url] = Object.entries(boardEntries)[0];
 
-  return new Response("ok");
+  const boardId = uuidv4();
+
+  await db
+    .insert(Board)
+    .values({
+      id: boardId,
+    })
+    .returning();
+  await db.insert(Sound).values({
+    id: uuidv4(),
+    url,
+    boardId,
+  });
+
+  return new Response(
+    JSON.stringify({
+      boardId,
+    })
+  );
 };
--- src/pages/boards/_components/NewBoardForm.tsx
+++ src/pages/boards/_components/NewBoardForm.tsx
@@ -1,20 +1,102 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "../../../components/ui/input";
+import { useState } from "react";
+import type { Sound } from "@/lib/schema";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export function NewBoardForm() {
-  return (
-    <form onSubmit={async (e) => {
-      e.preventDefault();
-      // @ts-expect-error They should've fixed form types 3 years ago
-      const formData = new FormData(e.target);
+  const [sounds, setSounds] = useState<Map<string, string>>(new Map());
 
-      console.log('TODO: validation');
-      
-      await fetch('/api/boards/new', {
-        method: 'POST',
-        body: formData,
-      });
-    }}>
-      <Input type="url" name="url" />
+  function addSound(url: string) {
+    setSounds((sounds) => {
+      const id = `${sounds.size}`;
+      sounds.set(id, url);
+      return new Map(sounds);
+    });
+  }
+
+  function updateSound(id: string, url: string) {
+    setSounds((sounds) => {
+      if (!sounds.has(id)) {
+        throw new Error(`Sound with id ${id} does not exist.`);
+      }
+      sounds.set(id, url);
+      return new Map(sounds);
+    });
+  }
+
+  return (
+    <>
+      <Button
+        onClick={async () => {
+          const res = await fetch("/api/boards/new", {
+            method: "POST",
+            body: JSON.stringify(Object.fromEntries(sounds.entries())),
+            headers: { "Content-Type": "application/json" },
+          });
+          // TODO: error banner
+          if (res.status !== 200) return;
+
+          const { boardId } = await res.json();
+          window.location.pathname = `/boards/${boardId}`;
+        }}
+      >
+        Create board 🎉
+      </Button>
+      <div className="grid grid-cols-3 gap-3">
+        {[...sounds.entries()].map(([id, url]) => (
+          <FormCard
+            key={id}
+            onSubmit={async (e) => {
+              e.preventDefault();
+              // @ts-expect-error They should've fixed form types 3 years ago
+              const formData = new FormData(e.target);
+
+              const updatedUrl = formData.get("url");
+              if (typeof updatedUrl !== "string") return;
+
+              updateSound(id, updatedUrl);
+            }}
+          >
+            <Input name="url" defaultValue={url} />
+            <Button variant="secondary">Update</Button>
+          </FormCard>
+        ))}
+        <FormCard
+          onSubmit={async (e) => {
+            e.preventDefault();
+            // @ts-expect-error They should've fixed form types 3 years ago
+            const formData = new FormData(e.target);
+
+            const url = formData.get("url");
+            if (typeof url !== "string") return;
+
+            addSound(url);
+
+            e.currentTarget.reset();
+          }}
+        >
+          <Input type="url" name="url" />
+          <Button>Add</Button>
+        </FormCard>
+      </div>
+    </>
+  );
+}
+
+interface FormCardProps extends React.FormHTMLAttributes<HTMLFormElement> {
+  children: React.ReactNode;
+}
+
+function FormCard({ children, ...props }: FormCardProps) {
+  return (
+    <form {...props}>
+      <Card>
+        <CardHeader>
+          <h2>My sound</h2>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-2">{children}</CardContent>
+      </Card>
     </form>
-  )
+  );
 }
