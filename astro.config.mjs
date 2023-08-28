0bd31bbf71ac8524d246d319756a513d814b3605
bholmesdev <bholmesdev@gmail.com>
2023-08-28 16:34:26 -0400

    deps: shadcn, vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  })],
+  output: "server",
+  adapter: vercel()
+});
