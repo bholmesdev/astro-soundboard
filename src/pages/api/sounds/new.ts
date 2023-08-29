c4f2966d20ef2a1fd2a54c8cf2c1af47fed69241
bholmesdev <bholmesdev@gmail.com>
2023-08-29 18:35:13 -0400

    fix: attach sound by board id, add updated label

--- src/pages/api/sounds/new.ts
+++ src/pages/api/sounds/new.ts
@@ -1,8 +1,21 @@
 import { Sound, db } from "@/lib/schema";
 import type { APIRoute } from "astro";
+import { z } from "zod";
 
-export const POST: APIRoute = async () => {
-  const [sound] = await db.insert(Sound).values({}).returning();
+const payload = z.object({
+  boardId: z.string().uuid(),
+});
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.json();
+  const { boardId } = payload.parse(data);
+
+  const [sound] = await db
+    .insert(Sound)
+    .values({
+      boardId,
+    })
+    .returning();
 
   return new Response(JSON.stringify(sound), {
     headers: { "Content-Type": "application/json" },
--- src/pages/boards/_components/SoundForm.tsx
+++ src/pages/boards/_components/SoundForm.tsx
@@ -14,12 +14,10 @@ import type { Sound } from "@/lib/schema";
 
 const queryClient = new QueryClient();
 
-type SoundFormProps = {
-  id: string;
-  url: string;
-};
+type SoundFormProps = Pick<typeof Sound.$inferSelect, "id" | "url">;
 
 type SoundsFormProps = {
+  boardId: string;
   initialSounds: (typeof Sound.$inferSelect)[];
 };
 
@@ -31,13 +29,14 @@ export function Sounds(props: SoundsFormProps) {
   );
 }
 
-function SoundsMutation({ initialSounds }: SoundsFormProps) {
+function SoundsMutation({ initialSounds, boardId }: SoundsFormProps) {
   const [sounds, setSounds] = useState(initialSounds);
 
   const soundAdd = useMutation({
     mutationFn: async () => {
       const res = await fetch(`/api/sounds/new`, {
         method: "POST",
+        body: JSON.stringify({ boardId }),
       });
       const data = await res.json();
       // surely type safety isn't important right?
@@ -48,7 +47,7 @@ function SoundsMutation({ initialSounds }: SoundsFormProps) {
   return (
     <>
       {sounds.map((s) => (
-        <SoundFormMutation key={s.id} id={s.id} url={s.url ?? ""} />
+        <SoundFormMutation key={s.id} {...s} />
       ))}
       <Button onClick={() => soundAdd.mutate()}>Add</Button>
     </>
@@ -60,22 +59,23 @@ const soundFormValidator = zfd.formData({
   url: zfd.text(z.string().url()),
 });
 
-function SoundFormMutation({ id: soundId, url }: SoundFormProps) {
+function SoundFormMutation({ id, url }: SoundFormProps) {
   const soundUpdate = useMutation({
-    mutationFn: (formData: FormData) => {
-      console.log("soundId", soundId);
-      return fetch(`/api/sounds/${soundId}`, {
+    mutationFn: async (formData: FormData) => {
+      return fetch(`/api/sounds/${id}`, {
         method: "PUT",
         body: formData,
       });
     },
   });
+  // TODO: standardized check for incomplete sound entry
+  const isComplete = url || soundUpdate.status === "success";
+
   const debounced = useDebouncedCallback((formData: FormData) => {
     const parsed = soundFormValidator.safeParse(formData);
     if (parsed.success) {
       soundUpdate.mutate(formData);
     }
-    // TODO: error handling for debounced input?
   }, 300);
 
   return (
@@ -87,11 +87,20 @@ function SoundFormMutation({ id: soundId, url }: SoundFormProps) {
       }}
     >
       <Card>
-        <CardHeader>
+        <CardHeader className="flex-row flex justify-between items-center gap-2">
           <Input className="text-2xl" type="text" name="name" />
+          {isComplete ? (
+            <p className="bg-green-300 text-xs text-green-950 px-2 py-1 rounded-full">
+              ✓ Updated
+            </p>
+          ) : (
+            <p className="bg-yellow-100 text-xs text-yellow-950 px-2 py-1 rounded-full">
+              ✗ Incomplete
+            </p>
+          )}
         </CardHeader>
         <CardContent className="flex flex-col gap-2">
-          <Input type="url" name="url" defaultValue={url} />
+          <Input type="url" name="url" defaultValue={url ?? ""} />
         </CardContent>
       </Card>
     </form>
