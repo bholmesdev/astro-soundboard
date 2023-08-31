38e0b76d9b5a64c76656e6b930deb48f437dbb45
bholmesdev <bholmesdev@gmail.com>
2023-08-31 18:41:25 -0400

    feat: log in and out charm

---
+++ src/pages/logout.ts
@@ -0,0 +1,16 @@
+import type { APIRoute } from "astro";
+import { auth } from "@/lib/lucia";
+
+export const POST: APIRoute = async (context) => {
+  const session = await context.locals.auth.validate();
+  console.log("session", session);
+  if (!session) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  await auth.invalidateSession(session.sessionId);
+  context.locals.auth.setSession(null);
+  return context.redirect("/", 302);
+};
