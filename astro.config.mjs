4b96a0af81225c7ecb43e8476be6a4b293e9b950
bholmesdev <bholmesdev@gmail.com>
2023-09-01 17:14:16 -0400

    fix: noExternal for dropzone

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,17 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    ssr: {
+      noExternal: ["@uploadthing/react"],
+    },
+  },
 });
