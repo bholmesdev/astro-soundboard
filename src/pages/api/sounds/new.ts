22bccdd2296398480c8703dcd99cb46bfd8d3048
bholmesdev <bholmesdev@gmail.com>
2023-09-01 16:37:37 -0400

    fix: security on new sound endpoint

--- src/pages/api/sounds/new.ts
+++ src/pages/api/sounds/new.ts
@@ -1,12 +1,20 @@
-import { Sound, db } from "@/lib/schema";
+import { Board, Sound, db } from "@/lib/schema";
 import type { APIRoute } from "astro";
+import { and, eq } from "drizzle-orm";
 import { z } from "zod";
 
 const payload = z.object({
   boardId: z.string().uuid(),
 });
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ request, locals }) => {
+  const session = await locals.auth.validate();
+  if (!session) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
   let data = {};
   try {
     data = await request.json();
@@ -17,6 +25,15 @@ export const POST: APIRoute = async ({ request }) => {
     });
   }
   const { boardId } = payload.parse(data);
+
+  const board = await db
+    .select({ boardId: Board.id })
+    .from(Board)
+    .where(and(eq(Board.userId, session.user.userId), eq(Board.id, boardId)));
+
+  if (board.length === 0) {
+    return new Response("Not found", { status: 404 });
+  }
 
   const [sound] = await db
     .insert(Sound)
